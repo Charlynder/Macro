2257f10b5f6082186d846bf65700a4e03b44c1e4
Christian Alvarez <Alvarez.christian46@outlook.com>
2021-11-04 19:36:22 -0500

    new functions
    perCapita(), exchangeRate(), findPopulation()
    
    newpercent() needs debug

--- gdpfinder.py
+++ gdpfinder.py
@@ -1,5 +1,5 @@
 '''
-GDP Finder API
+GDP Finder Library
 
 Author: Christian Alvarez
 Date: Nov 3, 2021
@@ -14,45 +14,65 @@ class GDPfinder:
         ## convert to percentage
         pGDP = float((DurableGoods / gdpReg) * 100)
 
-        return str(pGDP) + '% - $' + str(gdpReg)
+        return float(pGDP),  float(gdpReg)
 
     ## find the government spending amount 
     def govSpend(GDP, Consumpion, Investment, Exports, Imports):
         govSpend = float(GDP - Consumpion - Investment - (Exports - Imports))
 
-        return "$" + str(govSpend)
+        return float(govSpend)
 
     ## find the consumpion spending amount
     def consSpend(gdp, government, investment, exports, imports):
         consSpend = float(gdp - government - investment - (exports - imports))
 
-        return "$" + str(consSpend)
+        return float(consSpend)
 
     ## find the consumpion spending amount
     def investSpend(gdp, government, consumpion, exports, imports):
         investSpend = float(gdp - government - consumpion - (exports - imports))
 
-        return "$" + str(investSpend)
+        return float(investSpend)
 
     ## finds the total gdp 
     def totalGDP(Consumption, Investment, Government, Exports, Imports):
         ## calculate tGDP
         gdpTotal = float(Consumption + Investment + Government + (Exports - Imports))
 
-        return "$" + str(gdpTotal)
+        return float(gdpTotal)
 
     ## finds the real gdp 
     def realGDP(nominalGDP, priceIndex):
         ## find the real gdp
         realgdp = nominalGDP / (priceIndex / 100)
-        return "$" + str(round(realgdp, 1))
 
+        return float(realgdp)
+
+    ## finds the gdp per capita 
+    def perCapita(realgdp, population):
+        percapita = float(realgdp / population)
+
+        return percapita
+
+    ## finds the exchange rate 
+    def exchangerate(foreignGDP, foreignValue, usValue):
+        covertedgdp = float(foreignGDP / (foreignValue /usValue))
+
+        return covertedgdp
+
+    ## find the population based on the the gdp & the per capita amount
+    def findPopulation(realgdp, perCapita):
+        population = realgdp / perCapita
+        return population
+
+## needs debug
     ## finds the new percentage of the gdp
-    def newpercentGDP(new_gdp, old_gdp):
+    '''
+    def newpercent(new_gdp, old_gdp):
         ## find the new percentage
-        if new_gdp < old_gdp:
-            newGDPpercent = ((- new_gdp) / old_gdp) * 100
-        else:
-            newGDPpercent = ((new_gdp - old_gdp) / old_gdp) * 100
+        if new_gdp < old_gdp: ## if the old gdp is greater then the new gdp
+            
+        else: 
         
-        return str(int(newGDPpercent)) + "%"
+        return newGDPpercent
+    '''
